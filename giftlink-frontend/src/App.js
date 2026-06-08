03e42f3b954d37215fb12c4c1c730f09c39b6b33
Promise2010-tech <brilliantpromise0@gmail.com>
2026-06-08 11:06:32 +0200

    Fix: Integrated both SearchPage and Profile component routing paths inside App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,11 +4,11 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage'; 
-// TASK 1 HINT: Import your SearchPage component
 import SearchPage from './components/SearchPage/SearchPage'; 
+import Profile from './components/Profile/Profile';
+import Navbar from './components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const navigate = useNavigate();
@@ -21,8 +21,8 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          {/* TASK 1 HINT: Add the search page path route mapping layout */}
           <Route path="/app/search" element={<SearchPage />} />
+          <Route path="/app/profile" element={<Profile />} />
         </Routes>
         </>
   );
